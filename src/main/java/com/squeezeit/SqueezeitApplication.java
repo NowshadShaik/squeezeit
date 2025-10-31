1cc5f65d371b0578bb16260021cf2156247b2012
Nowshad Shaik <nowshad7037@gmail.com>
2025-10-31 22:35:49 +0530

    Initial commit

---
+++ src/main/java/com/squeezeit/SqueezeitApplication.java
@@ -0,0 +1,13 @@
+package com.squeezeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqueezeitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqueezeitApplication.class, args);
+	}
+
+}
